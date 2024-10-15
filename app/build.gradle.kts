72fd183e98b7adff343dd28fe0aca2275977aa71
Viktor Zahariev <viktor.zahariev@wtmeters.local>
2024-10-15 13:11:57 +0200

    Added scrolling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("com.google.android.material:material:1.13.0-alpha06")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
